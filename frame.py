9d47843c9fad74a5915737fc384243b7d81c2064
therobingan <robingan@robins-mbp.lan>
2021-10-21 00:27:40 -0700

    semi work

--- frame.py
+++ frame.py
@@ -1,23 +1,19 @@
-import cv2
-from skimage.metrics import mean_squared_error
-from skimage.metrics import structural_similarity as ssim
 import math
+import random
 
 MERGE_DISTANCE = 15
 MERGE_AREA = 100 #to be determined later
+MIN_DISTANCE_OF_CLOSE = 50 #change to factor of the box itself's width and height
+INACTIVE_LIMIT = 5
 
-class Frame:
-    def __init__(self, frameNumber):
-        self.number = frameNumber
-        self.boxes = []
-    def add(self, boxData):
-        self.boxes.append(boxData)
-    def getBoxes(self):
-        return self.boxes
-    def setBoxes(self, newBoxes):
-        self.boxes = newBoxes
-    def getFrameNum(self):
-        return self.number
+class Box:
+    def __init__(self, data, frame):
+        self.data = data
+        self.frame = frame
+    def frameNumber(self):
+        return self.frame
+    def size(self):
+        return self.data
 
 class Dive:
     def __init__(self):
@@ -27,15 +23,31 @@ class Dive:
         self.points.append(value)
     def size(self):
         return len(self.points)
+    def lastBox(self) -> Box:
+        return self.points[-1]
+    def isActive(self, currentFrame) -> bool:
+        if (currentFrame - self.lastBox().frameNumber() > INACTIVE_LIMIT):
+            return False
+        return True
+    def boxes(self) -> list:
+        return self.points
 
-def mergeBoxes(boxesIn):
-    boxes = list(boxesIn)
+def randomColor():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    return [r,g,b]
+
+def mergeBoxes(boxesIn:list):
+    boxes = boxesIn.copy()
     i = 0
+
     for curr in boxes:
-        if(i + 1 < len(boxes)):
+        if (i + 1 < len(boxes)):
             j = i + 1
+
             for compare in boxes[i+1:]:
-                if(canMerge(curr, compare)):
+                if (canMerge(curr, compare)):
                     (x, y, w, h) = curr
                     (x2, y2, w2, h2) = compare
                     boxes.pop(j)
@@ -46,20 +58,13 @@ def mergeBoxes(boxesIn):
                 else:
                     j += 1
         i += 1
+    boxesIn = boxes
     return boxes
 
 def canMerge(box1, box2):
-    (x, y, w, h) = box1
+    (x1, y1, w1, h1) = box1
     (x2, y2, w2, h2) = box2
-    return real_distance((x, y, x+w, y+h), (x2, y2, x2+w2, y2+h2)) < MERGE_DISTANCE and min(w2*h2, w*h) < MERGE_AREA
-
-def extract(frames):
-    result = []
-    for i in range(len(frames) - 1):
-        current = frames[i]
-        next = frames[i + 1]
-        for f in current:
-            pass
+    return real_distance(box1, box2) < MERGE_DISTANCE and min(w2*h2, w1*h1) < MERGE_AREA
 
 #only used in real_distance
 def euclidean_distance(box1, box2):
@@ -68,8 +73,9 @@ def euclidean_distance(box1, box2):
     return math.hypot(x2 - x, y2 - y)
 
 def real_distance(box1, box2):
-    (x1, y1, x1b, y1b) = box1
-    (x2, y2, x2b, y2b) = box2
+    (x1, y1, w1, h1) = box1
+    (x2, y2, w2, h2) = box2
+    x1b, x2b, y1b, y2b = x1+w1, x2+w2, y1+h1, y2+h2
     left = x2b < x1
     right = x1b < x2
     bottom = y2b < y1
@@ -92,3 +98,64 @@ def real_distance(box1, box2):
         return y2 - y1b
     else:             # rectangles intersect
         return 0.
+
+def isSame(box1, box2):
+    (x, y, w, h) = box1
+    (x2, y2, w2, h2) = box2
+    factor = euclidean_distance((x ,y), (x2, y2)) + abs(w2-w) + abs(h2-h)
+    #print(factor)
+    return factor < 100
+
+def removeSameBox(frame1: list, frame2: list):
+    if (len(frame1) > 0):
+        removeIndex = []
+        for box in frame2:
+            removeIndex += [i for i, box2 in enumerate(frame1) if (isSame(box2, box))]
+        frame1 = [ele for i, ele in enumerate(frame1) if i not in removeIndex]
+
+def addDive(dives:list, boxData, frameNum):
+    newDive = Dive()
+    newDive.add(Box(boxData, frameNum))
+    dives.append(newDive)
+
+def extract(frames):
+    active = []
+    all = []
+    for i in range(len(frames) - 1):
+        current = frames[i]
+        next = frames[i+1]
+        removeSameBox(current, next)
+        frames[i] = current
+
+        if (i == 0):
+            current = mergeBoxes(current)
+            for box in current:
+                addDive(active, box, i)
+        else:
+            if (len(current) > 0):
+                picked = set([])
+                for (i2, dive) in enumerate(active):
+                    distances = [real_distance(dive.lastBox().size(), b) for b in current]
+                    minDistance = min(distances)
+                    if (minDistance < MIN_DISTANCE_OF_CLOSE):
+                        minIndex = distances.index(minDistance)
+                        picked.add(minIndex)
+                        minBox = current[minIndex]
+                        mergedSurrounding = mergeBoxes([b for b in current if canMerge(minBox, b)] + [minBox])
+                        active[i2].add(Box(mergedSurrounding[0], i))
+
+            #add non pick as new dive
+            for ele in [box_ for i5, box_ in enumerate(current) if i5 not in picked]:
+                addDive(active, ele, i)
+
+            #remove inactive
+            inactiveIndex = [i3 for i3, dive in enumerate(active) if not dive.isActive(i)]
+            for index in inactiveIndex:
+                all.append(active[index])
+
+            active = [d for i6, d in enumerate(active) if i6 not in inactiveIndex]
+
+    for dive in active:
+        all.append(dive)
+
+    return all
--- tracker.py
+++ tracker.py
@@ -6,7 +6,7 @@ from frame import *
 
 tracemalloc.start()
 
-VIDEO_PATH = 'video/train/test2.mp4'
+VIDEO_PATH = 'video/train/test3.mp4'
 BOX_BORDER_COLOR = (111, 0, 51)
 
 cap = cv2.VideoCapture(VIDEO_PATH)
@@ -38,7 +38,7 @@ frames = []
 frameNum = 1
 
 while cap.isOpened():
-    currentFrame = Frame(frameNumber=frameNum)
+    currentFrame = []
 
     diff = cv2.absdiff(s1, s2)
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
@@ -57,7 +57,7 @@ while cap.isOpened():
             
     #merged = mergeBoxes(raw)
     merged = raw
-    currentFrame.setBoxes(merged)
+    currentFrame = merged
     for b in merged:
         (x, y, w, h) = b
         cv2.rectangle(frame1, (x+widthDownLimit, y+heightDownLimit), 
@@ -85,12 +85,31 @@ current, peak = tracemalloc.get_traced_memory()
 print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
 tracemalloc.stop()
 
+dives = extract(frames)
 #draw the coordinates
-for frame in frames:
-    for box in frame.getBoxes():
+'''for i, frame in enumerate(frames):
+    for box in frame:
         (x, y, w, h) = box
         cv2.rectangle(firstFrame, (x+widthDownLimit, y+heightDownLimit), 
                     (x+w+widthDownLimit, y+h+heightDownLimit), BOX_BORDER_COLOR, 2)
+    #cv2.imwrite("images/coordinates_" + str(i) + ".jpg", firstFrame)'''
+
+lenList = [len(dive.boxes()) for index, dive in enumerate(dives)]
+maxLen = dives[lenList.index(max(lenList))]
+#dives = [maxLen]
+for index, dive in enumerate(dives):
+    color = randomColor()
+    prev = None
+    print(len(dive.boxes()))
+    for d in dive.boxes():
+        (x, y, w, h) = d.size()
+        cv2.rectangle(firstFrame, (x+widthDownLimit, y+heightDownLimit), (x+w+widthDownLimit, y+h+heightDownLimit), 
+                    (color[0], color[1], color[2]), 2)
+        if (prev != None):
+            (x2, y2, w2, h2) = prev.size()
+            cv2.line(firstFrame, (x2+int(w2/2), y2+int(h2/2)), (x+int(w/2), y+int(h/2)), 
+                    (color[0], color[1], color[2]), thickness=3, lineType=8)
+        prev = d
 
 cv2.imwrite("coordinates" + VIDEO_PATH[16]+ ".jpg",firstFrame)
 cv2.destroyAllWindows()
